58e4ddd21ce086587100bc07a883903c4b3cc8d0
Alec Stassforth <astassforthh@gmail.com>
2025-08-05 13:02:33 -0400

    commit 8/5

--- gdm_histo_comp.py
+++ gdm_histo_comp.py
@@ -1,8 +1,8 @@
 import ROOT
-# code that puts data from gluing trials into histogram; overlays 3 data sets
+# code that puts data from gluing trials into histogram; overlays 2 data sets
 c = ROOT.TCanvas("c", "", 600, 500)
-hist1 = ROOT.TH1F("GDM", "Normalized Mass of Glue Deposited (mg)", 100, 2, 11)
-hist2 = ROOT.TH1F("TECH", "TECH", 100, 2, 11)
+hist1 = ROOT.TH1F("GDM", "Normalized Mass of Glue Deposited (mg)", 10, 2, 11)
+hist2 = ROOT.TH1F("TECH", "TECH", 10, 2, 11)
 # filling histogram with data
 with open("gdm_data_part.txt", "r") as file1:
     for line in file1:
@@ -10,7 +10,6 @@ with open("gdm_data_part.txt", "r") as file1:
 
 hist1.SetLineColor(616) # magenta
 hist1.Scale(1.0 / hist1.Integral())
-hist1.Draw("HIST")
 
 with open("garrett_data.txt", "r") as file2:
     for line in file2:
@@ -18,12 +17,15 @@ with open("garrett_data.txt", "r") as file2:
 
 hist2.SetLineColor(418) # green
 hist2.Scale(1.0 / hist2.Integral())
-hist2.Draw("HIST SAME")
-
+# so that no bins touch ceiling
+max_bin = hist2.GetMaximum()
+hist1.SetMaximum(max_bin * 1.1)
+# creating legend
 legend = ROOT.TLegend(.15, .7, .3, .85)
 legend.AddEntry(hist1, "GDM")
 legend.AddEntry(hist2, "TECH")
-
+hist1.Draw("HIST")
+hist2.Draw("HIST SAME")
 legend.Draw("SAME")
 
 ROOT.gApplication.Run()
--- gdm_histo_full.py
+++ gdm_histo_full.py
@@ -1,7 +1,7 @@
 import ROOT
 
 c = ROOT.TCanvas("c", "", 600, 500)
-hist = ROOT.TH1F("GDM", "Mass of Glue Deposited (mg)", 100, 4, 10)
+hist = ROOT.TH1F("GDM", "Mass of Glue Deposited (mg)", 12, 4, 10.3)
 
 with open("gdm_data_full.txt", "r") as file:
     for line in file:
@@ -9,7 +9,7 @@ with open("gdm_data_full.txt", "r") as file:
 
 hist.SetLineColor(ROOT.kRed)
 
-legend = ROOT.TLegend(.15, .7, .3, .85)
+legend = ROOT.TLegend(.15, .75, .27, .85)
 legend.AddEntry(hist, "GDM")
 
 hist.Draw("HIST")
